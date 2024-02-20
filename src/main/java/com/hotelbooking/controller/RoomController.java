4f5b9ec84cca08bac6e0978f95c6a4532ebfe88b
sandesh <bhujbalsandesh52@gmail.com>
2024-02-20 23:27:57 +0530

    create getAllRooms API in RoomController.java

--- src/main/java/com/hotelbooking/controller/RoomController.java
+++ src/main/java/com/hotelbooking/controller/RoomController.java
@@ -34,4 +34,20 @@ public class RoomController {
     public List<String> getRoomTypes(){
         return roomService.getAllRoomTypes();
     }
+
+    @GetMapping("/all-rooms")
+    public ResponseEntity<List<RoomResponse>> getAllRooms() throws SQLException {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for (Room room : rooms) {
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if (photoBytes != null && photoBytes.length > 0) {
+                String base64Photo = Base64.encodeBase64String(photoBytes);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(base64Photo);
+                roomResponses.add(roomResponse);
+            }
+        }
+        return ResponseEntity.ok(roomResponses);
+    }
 }
