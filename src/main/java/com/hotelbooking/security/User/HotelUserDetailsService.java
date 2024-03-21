52afc934ab313bf9d7b0f4e65e3c380a699c11fa
sandesh <bhujbalsandesh52@gmail.com>
2024-03-21 15:38:12 +0530

    add hotel user details class and service class for the role functionality

---
+++ src/main/java/com/hotelbooking/security/User/HotelUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.hotelbooking.security.User;
+
+import com.hotelbooking.model.User;
+import com.hotelbooking.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User NOt Found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
